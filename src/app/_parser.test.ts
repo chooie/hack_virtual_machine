e3f6304cfc0618417f2c023e7ac4ffc5cc9208ec
Charlie Hebert <charlie.hebert92@gmail.com>
2023-01-10 00:39:01 +0000

    Start parser

--- src/app/_parser.test.ts
+++ src/app/_parser.test.ts
@@ -1,8 +1,84 @@
-import { assertStrictEquals } from "@test_deps/assert.ts";
+import { assertEquals, assertThrows } from "@test_deps/assert.ts";
 import { describe, it } from "@test_deps/bdd.ts";
 
-describe("Test", () => {
-  it("is true", () => {
-    assertStrictEquals(true, true);
+import * as parser from "./parser.ts";
+
+/*
+BasicTest.vm
+
+push constant 10
+pop local 0
+push constant 21
+push constant 22
+pop argument 2
+pop argument 1
+push constant 36
+pop this 6
+push constant 42
+push constant 45
+pop that 5
+pop that 2
+push constant 510
+pop temp 6
+push local 0
+push that 5
+add
+push argument 1
+sub
+push this 6
+push this 6
+add
+sub
+push temp 6
+add
+*/
+
+describe("Parser", () => {
+  it("parses pushes", () => {
+    assertEquals(parser.parse("push constant 10"), {
+      command: "push",
+      segment: "constant",
+      value: 10,
+    });
+
+    assertEquals(parser.parse("push local 0"), {
+      command: "push",
+      segment: "local",
+      value: 0,
+    });
+  });
+
+  it("parses pops", () => {
+    assertEquals(parser.parse("pop local 0"), {
+      command: "pop",
+      segment: "local",
+      value: 0,
+    });
+
+    assertEquals(parser.parse("pop argument 2"), {
+      command: "pop",
+      segment: "argument",
+      value: 2,
+    });
+  });
+
+  it("parses arithmetic/logical commands", () => {
+    assertEquals(parser.parse("add"), {
+      command: "add",
+    });
+
+    assertEquals(parser.parse("sub"), {
+      command: "sub",
+    });
+  });
+
+  it("fails on bad commands", () => {
+    assertThrows(
+      () => {
+        parser.parse("foo");
+      },
+      Error,
+      "Command not implemented, 'foo'",
+    );
   });
 });
--- src/app/parser.ts
+++ src/app/parser.ts
@@ -1,1 +1,103 @@
 // Parses each VM command into its lexical elements
+
+const arithmeticOrLogicalCommands = [
+  "add",
+  "sub",
+  "neg",
+  "eq",
+  "gt",
+  "lt",
+  "and",
+  "or",
+  "not",
+] as const;
+type ArithmeticOrLogicalCommand = typeof arithmeticOrLogicalCommands[number];
+const commands = ["push", "pop", ...arithmeticOrLogicalCommands] as const;
+type Command = typeof commands[number];
+
+const segments = [
+  "local",
+  "argument",
+  "this",
+  "that",
+  "constant",
+  "static",
+  "pointer",
+  "temp",
+] as const;
+type Segment = typeof segments[number];
+
+type PushOrPopCommandResult = {
+  command: "push" | "pop";
+  segment: Segment;
+  value: number;
+};
+
+type ArithmeticOrLogicalCommandResult = {
+  command: ArithmeticOrLogicalCommand;
+};
+
+export function parse(
+  lineCommand: string,
+): PushOrPopCommandResult | ArithmeticOrLogicalCommandResult {
+  const words = lineCommand.split(" ");
+  const command = words[0];
+
+  if (!isAValidCommand(command)) {
+    throw new Error(`Command not implemented, '${command}'`);
+  }
+
+  if (command === "push") {
+    const segment = words[1];
+    const value = words[2];
+
+    if (!isAValidSegment(segment)) {
+      throw new Error(`Not a valid segment, ${segment}`);
+    }
+
+    return {
+      command: "push",
+      segment,
+      value: parseInt(value),
+    };
+  }
+
+  if (command === "pop") {
+    const segment = words[1];
+    const value = words[2];
+
+    if (!isAValidSegment(segment)) {
+      throw new Error(`Not a valid segment, ${segment}`);
+    }
+
+    return {
+      command: "pop",
+      segment,
+      value: parseInt(value),
+    };
+  }
+
+  if (isAValidArithmeticOrLogicalCommand(command)) {
+    return {
+      command,
+    };
+  }
+
+  throw new Error("Something went wrong");
+}
+
+function isAValidCommand(command: string): command is Command {
+  return commands.includes(command as Command);
+}
+
+function isAValidArithmeticOrLogicalCommand(
+  command: string,
+): command is ArithmeticOrLogicalCommand {
+  return arithmeticOrLogicalCommands.includes(
+    command as ArithmeticOrLogicalCommand,
+  );
+}
+
+function isAValidSegment(segment: string): segment is Segment {
+  return segments.includes(segment as Segment);
+}
